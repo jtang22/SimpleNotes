d6d0bfff54d7359d8f7192e4a768cb2a32241f17
Jordan Tang <jordantang0229@gmail.com>
2016-11-05 20:51:02 -0700

    Finished setting up content provider for SQLite DB

--- SimpleNotes/app/src/main/java/com/example/simplenotes/MainActivity.java
+++ SimpleNotes/app/src/main/java/com/example/simplenotes/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.simplenotes;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        insertNote("New note");
+    }
+
+    private void insertNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.NOTE_TEXT, noteText);
+        Uri noteUri = getContentResolver().insert(NotesProvider.CONTENT_URI, values);
+
+        Log.d("MainActivity", "Inserted note " + noteUri.getLastPathSegment());
     }
 
     @Override
